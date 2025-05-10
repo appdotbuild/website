5ad54106a1eb989f9270b12367a0907b30c6d4c8
Ditadi <victor.ditadi@neon.tech>
2025-05-10 10:29:34 +0100

    feat: add nextjs middleware that redirect all requests about auth to admin.app.build

--- middleware.ts
+++ middleware.ts
@@ -4,36 +4,22 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const { pathname, search } = request.nextUrl;
   
-  if (pathname.startsWith('/api/auth')) {
-    const rewriteUrl = new URL(`${pathname}${search}`, 'https://admin.app.build');
-    return NextResponse.rewrite(rewriteUrl);
-  }
-  
-  if (pathname.startsWith('/api/')) {
-    const rewriteUrl = new URL(`${pathname}${search}`, 'https://admin.app.build');
-    return NextResponse.rewrite(rewriteUrl);
-  }
-  
-  if (pathname.startsWith('/_next/')) {
-    const rewriteUrl = new URL(`${pathname}${search}`, 'https://admin.app.build');
-    return NextResponse.rewrite(rewriteUrl);
-  }
-  
   if (pathname.startsWith('/handler')) {
     const rewriteUrl = new URL(`${pathname}${search}`, 'https://admin.app.build');
-    const response = NextResponse.rewrite(rewriteUrl);
-    
-    return response;
+    return NextResponse.rewrite(rewriteUrl);
   }
+  
+  if (pathname.startsWith('/api/auth/cli')) {
+    const rewriteUrl = new URL(`${pathname}${search}`, 'https://admin.app.build');
+    return NextResponse.rewrite(rewriteUrl);
+  }
+  
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
     '/handler/:path*',
-    '/api/:path*',
-    '/api/auth/:path*',
-    '/_next/static/:path*',
-    '/_next/image/:path*',
-    '/_next/data/:path*'
+    '/api/auth/cli/:path*',
   ],
 };
