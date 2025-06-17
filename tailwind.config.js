92c5cc264fab992b17b7672316da8437cbe8f24d
Raman Yasel <raman.yasel@gmail.com>
2025-06-17 12:23:34 +0300

    feat: upd home illustration and accent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -94,7 +94,7 @@ module.exports = {
         94: 'hsl(220 10% 94%)',
         98: 'hsl(210 20% 98%)',
       },
-      blue: '#6388EE',
+      blue: '#1351FF',
       orange: '#FF8126',
     }),
     extend: {
